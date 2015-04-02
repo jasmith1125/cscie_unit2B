d735987d9697fddeb784b7e9d1b599c248b92a71
jasmith1125 <jasmith1125@yahoo.com>
2015-04-01 23:48:03 -0400

    stil trying

--- js/unit2b.js
+++ js/unit2b.js
@@ -1,201 +1,82 @@
-var eventDescriptionEl = document.getElementById("eventDescription");
-var eventDateEl = document.getElementById("eventDate");
-var whoEl = document.getElementById("who");
-var notesEl = document.getElementById("notes");
-var saveEl = document.getElementById("saveBtn");
-var outputEl = document.getElementById("output");
+// Create an array to hold all of the square objects
+var events = [];
 
-//window.localStorage.clear();
-
-
-var data = [];
-
+// Create a constructor function that enables creation of event
 var Event = function(eventDescription, eventDate, who, notes) {
     this.eventDescription = eventDescription;
     this.eventDate = eventDate;
     this.who = who;
     this.notes = notes;
-    this.showEvent = function() {
-    return this.eventDescription + " " + this.eventDate + " " + this.who + " " +  this.notes;
 }
- 
-};
 
-Event.prototype.createDiv = function() {
+function writeToPage(anEvent) {
+	var myOutput = document.getElementById("output");
+
 	var div = document.createElement("div");
-	var eventDescriptionNode = document.createTextNode(this.eventDescription);
-	div.appendChild(eventDescriptionNode);
-	var eventDateNode = document.createTextNode(this.eventDate);
-	div.appendChild(eventDateNode);
-	var whoNode = document.createTextNode(this.who);
-	div.appendChild(whoeode);
-	var notesNode = document.createTextNode(this.notes);
-	div.appendChild(notesNode);
+
+		var eventDescriptionNode = document.createTextNode(anEvent.eventDescription);
+		var span = document.createElement("span"); // create a new span tag
+		span.appendChild(eventDescriptionNode); // add the text node to the span
+		div.appendChild(span);
+
+		var eventDateNode = document.createTextNode(anEvent.eventDate);
+		var span = document.createElement("span");
+		span.appendChild(eventDateNode);
+		div.appendChild(span);
+
+		var whoNode = document.createTextNode(anEvent.who);
+		var span = document.createElement("span"); // create a new span tag
+		span.appendChild(whoNode); // add the text node to the span
+		div.appendChild(span);
+
+
+		var notesNode = document.createTextNode(anEvent.notes);
+		var span = document.createElement("span"); // create a new span tag
+		span.appendChild(notesNode);
+		div.appendChild(span);
+
+		// Add form entries to "output" div
+		myOutput.appendChild(div);
+
 }
 
-saveEl.onclick = function() {
-    var eventDescription = eventDescriptionEl.value;
-    var eventDate = eventDateEl.value;
-    var who = whoEl.value;
-    var notes = notesEl.value;
-    /*
-    var obj = {
-        name: name,
-        color: color
-    };
-    */
-    var event = new Event(eventDescription, eventDate, who, notes);
-    data.push(event);
-    updateLS();
-    updateOutput();
-    clearInputs();
-};
+function submitEvent() {
+	// Get values
+	var eventDescription = document.getElementById("eventDescription").value;
+	var eventDate = document.getElementById("eventDate").value;
+	var who = document.getElementById("who").value;
+	var notes = document.getElementById("notes").value;
 
-var initLS = function() {
-	dataLS = JSON.parse(window.localStorage.getItem("data"));
-	for(i in dataLS) {
-		var obj = dataLS[i];
-		var event = new Event(obj.eventDescription, obj.eventDate, obj.who, obj.notes);
-		data.push(event);
-	}
-	updateOutput();
+	// Write the event object to teh page
+	var myEvent = new Event(eventDescription, eventDate, who, notes);
+	writeToPage(myEvent);
+
+	// Add event to an array of events
+	events.push(myEvent);
+
+	// Write the array to local storage
+	var jsonString = JSON.stringify(events);
+
+	localStorage.setItem("eventStorage", jsonString);
+
 }
 
-var clearInputs = function() {
-	eventDescriptionEl.value = '';
-	eventDateEl.value = '';
-	whoEl.value = '';
-	notesEl.value = '';
-};
-
-var updateLS = function() {
-	window.localStorage.setItem("data", JSON.stringify(data));
-};
-
-var initLS = function() {
-	data = JSON.parse(window.localStorage.getItem("data"));
-	updateOutput();
-};
-
-console.log(window.localStorage);
-
-// Check for events in local storage
 window.onload = function() {
-var persistedData = window.localStorage.getItem('data');
-if (persistedData != undefined) {
-	persistedData = JSON.parse(persistedData);
-	console.log('JSON.parse(persistedData):');
-	console.log(persistedData);
-	console.log(' ');
-    // Load events from local storage into array (as objects)
-	persistedData.forEach(function(d) {
-		data.push(new Event(d.eventDescription, d.eventDate, d.who, d.notes));
+	// Assign an onclick handler to the "save" button
+	document.getElementById("saveBtn").onclick = submitEvent;
 
-		});
-		updateOutput();
-	
-}
-}
-var updateOutput = function(event) {
-	outputEl.innerHTML = '';
-	for(i in data) {
-		var event = data[i];
-		var div = document.createElement("div");
-		var eventDescriptionNode = document.createTextNode(event.eventDescription);
-		div.appendChild(eventDescriptionNode);
-		var eventDateNode = document.createTextNode(event.eventDate);
-		div.appendChild(eventDateNode);
-		var whoNode = document.createTextNode(event.who);
-		div.appendChild(whoNode);
-		var notesNode = document.createTextNode(event.notes);
-		div.appendChild(notesNode);
-		outputEl.appendChild(div);
+	// Get all events from local storage and store in the global array
+	var jsonString = localStorage.getItem("eventStorage");
+	if (jsonString) {
+		events = JSON.parse(jsonString);
 	}
-} 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Set variables to collect form data
-var occasionEl = document.getElementById("occasion");
-var dateEl = document.getElementById("date");
-var personEl = document.getElementById("person");
-var notesEl = document.getElementById("notes");
-var saveEl = document.getElementById("saveEvent")
-
-// Array to hold event objects
-var events = [];
-
-// Constructor function allows creation of events
-function Event(occasion, person, date, notes) {
-    this.occasion = occasion;
-    this.date = date;
-    this.person = person;
-    this.notes = notes;
+	 // Write the event objects to the page
+	 for (var i = 0; i < events.length; i++) {
+	 	writeToPage(events[i]);
+	 }
 }
 
-// Create onclick function to gather form data when button is activated
-saveEl.onclick = function() {
-	var event = new Event(occasion, date, person, notes);
-	events.push(event);
-};
-
-// Add events to the array
-events.push(new Event("birthday", "Quinn", "March 13", "party at Craig's Cruisers"));
-events.push(new Event("concert", "Chloe", "May 15", "E.K. Fine Arts Center"));
-
-//print events to console
-//console.log(events);
-
-// Convert event object to a string
-var jsonEvent = JSON.stringify(events);
-console.log('JSON.stringify(events):');
-console.log(jsonEvent);
-//console.log(' ');
-
-// Store JSON string in local storage
-window.localStorage.setItem("events", jsonEvent);
-
-// Clear out events that currently are in the array
-window.localStorage.clear();
-//events = [];
-//console.log(events);
-//console.log(' ');
-
-// Check for events in local storage
-var persistedEvents = window.localStorage.getItem('events');
-if (persistedEvents != undefined) {
-	persistedEvents = JSON.parse(persistedEvents);
-	//console.log('JSON.parse(persistedEvents):');
-	//console.log(persistedEvents);
-	//console.log(' ');
-
-	// Load events from local storage into array (as objects)
-	persistedEvents.forEach(function(e) {
-		events.push(new Event(e.occasion, e.date, e.person, e.notes));
-
-		console.log('Key / Value pairs:');
-		for (var key in e) {
-			console.log("The event's " + key + "is " + e[key]);
-		}
-		console.log(' ');
-	});
-}
-
-*/
+ // Clear form field inputs after submit
+	document.getElementById("eventForm").reset();
 
